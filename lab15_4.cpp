722de7edc0d2f165f819abb40298aa2ccb043c82
ittzzzmmm <ittzzzmmm@gmail.com>
2025-01-29 16:30:50 +0700

    ittzm

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -19,3 +19,16 @@ int main(){
 }
 
 //Do not modify source code above this line
+
+void shuffle(int &a,int &b,int &c,int &d){
+	int arr[4]={a,b,c,d};
+	for(int i=3;i>0;i--){
+		int j=rand()%(i+1);
+		swap(arr[i],arr[j]);
+	}
+	a=arr[0];
+	b=arr[1];
+	c=arr[2];
+	d=arr[3];
+}
+

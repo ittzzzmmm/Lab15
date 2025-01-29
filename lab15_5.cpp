3139a57451e1085f162d411cbd796c52d61188de
ittzzzmmm <ittzzzmmm@gmail.com>
2025-01-29 17:46:04 +0700

    ittzm

--- lab15_5.cpp
+++ lab15_5.cpp
@@ -20,3 +20,15 @@ int main(){
 
 //Do not modify the code above this line
 //Write definition of shuffle() using pointer after this line. 
+
+void shuffle(int *a,int *b,int *c,int *d){
+	int arr[4]={*a,*b,*c,*d};
+	for(int i=3;i>0;i--){
+		int j=rand()%(i+1);
+		swap(arr[i],arr[j]);
+	}
+	*a=arr[0];
+	*b=arr[1];
+	*c=arr[2];
+	*d=arr[3];
+}
